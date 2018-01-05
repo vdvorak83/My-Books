fe2e3c1d7f258636b00816961a64c18b3f5f1758
Bulat <jerdys.b@gmail.com>
2018-01-05 22:59:12 +0300

    admin page edited

--- src/main/java/ru/itis/controllers/AdminController.java
+++ src/main/java/ru/itis/controllers/AdminController.java
@@ -1,13 +1,19 @@
 package ru.itis.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import ru.itis.models.User;
 import ru.itis.services.AdminService;
+import ru.itis.services.AuthenticationService;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 @RequestMapping("/admin")
@@ -15,9 +21,15 @@ public class AdminController {
     @Autowired
     AdminService adminService;
 
+    @Autowired
+    AuthenticationService authenticationService;
+
     @GetMapping("/users")
-    public String getMainAdminPage(@ModelAttribute("model") ModelMap model) {
+    public String getMainAdminPage(@ModelAttribute("model") ModelMap model, Authentication authentication) {
         model.addAttribute("users", adminService.getAllUsers());
+        model.addAttribute(authenticationService.getUserByAuthentication(authentication));
+
+        System.out.println(adminService.getAllUsers());
 
         return "admin";
     }
